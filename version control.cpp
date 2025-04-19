9f7f9c11108066bf6a1080a067c0fa0a9ee36178
syrthno <syrthnogalantygalanty@gmail.com>
2025-04-19 14:59:56 +0500

    My biggest sin is programming

--- version control.cpp
+++ version control.cpp
@@ -6,12 +6,9 @@ int main()
     cout << "Hello World!\n";
 	cout << "Hello World!\n";
 	cout << "This is a test for version control.\n";
-<<<<<<< HEAD
 	cout << "ninja";
-=======
 	cout << "This is a test for version control.\n";
 	cout << "This is a test for version control.\n";
 	cout << "This is a test for version control.\n";
 	return 0;
->>>>>>> testing-branch
 }
